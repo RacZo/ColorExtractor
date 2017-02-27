79722b1758ed6d9990d48a53747a2c97f2986e05
Oscar S <oscar@realactionsoft.com>
2017-02-26 21:29:01 -0500

    Started fixing permissions Bumped up version

--- app/src/main/java/com/oscarsalguero/colorextractor/MainActivity.java
+++ app/src/main/java/com/oscarsalguero/colorextractor/MainActivity.java
@@ -26,12 +26,17 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.provider.MediaStore;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.graphics.Palette;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -47,8 +52,9 @@ import java.util.List;
  */
 public class MainActivity extends AppCompatActivity {
 
-    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
     private static final String LOG_TAG = MainActivity.class.getName();
+
+    private CoordinatorLayout coordinatorLayout;
     private ImageView imageViewInput;
     private TextView textViewColorVibrant;
     private TextView textViewColorMutedDark;
@@ -59,19 +65,29 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewSwatchMuted;
     private TextView textViewSwatchMutedLight;
     private TextView textViewSwatchMutedDark;
-    private ActionBar actionBar;
     private Uri outputFileUri;
     private static final String EMPTY_SPACE = " ";
+
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+
+    /**
+     * Id to identify a camera permission request.
+     */
+    private static final int REQUEST_CAMERA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setHomeButtonEnabled(true);
-            actionBar.setHomeAsUpIndicator(R.mipmap.ic_launcher);
-        }
+
+        supportInvalidateOptionsMenu();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.mipmap.ic_launcher);
+
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinator_layout);
         imageViewInput = (ImageView) findViewById(R.id.image_view_input);
         textViewColorVibrant = (TextView) findViewById(R.id.text_view_vibrant);
         textViewColorMutedDark = (TextView) findViewById(R.id.text_view_muted_dark);
@@ -248,6 +264,67 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    /**
+     * Called when the 'show camera' button is clicked.
+     * Callback is defined in resource layout definition.
+     */
+    public void showCamera(View view) {
+        Log.i(LOG_TAG, "Show camera button pressed. Checking permission.");
+        // BEGIN_INCLUDE(camera_permission)
+        // Check if the Camera permission is already available.
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Camera permission has not been granted.
+
+            requestCameraPermission();
+
+        } else {
+
+            // Camera permissions is already available, show the camera preview.
+            Log.i(LOG_TAG,
+                    "CAMERA permission has already been granted. Displaying camera preview.");
+            // showCameraPreview();
+        }
+        // END_INCLUDE(camera_permission)
+
+    }
+
+    /**
+     * Requests the Camera permission.
+     * If the permission has been denied previously, a SnackBar will prompt the user to grant the
+     * permission, otherwise it is requested directly.
+     */
+    private void requestCameraPermission() {
+        Log.i(LOG_TAG, "CAMERA permission has NOT been granted. Requesting permission.");
+
+        // BEGIN_INCLUDE(camera_permission_request)
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                android.Manifest.permission.CAMERA)) {
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // For example if the user has previously denied the permission.
+            Log.i(LOG_TAG,
+                    "Displaying camera permission rationale to provide additional context.");
+            Snackbar.make(coordinatorLayout, R.string.permission_camera_rationale,
+                    Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.ok, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{android.Manifest.permission.CAMERA},
+                                    REQUEST_CAMERA);
+                        }
+                    })
+                    .show();
+        } else {
+
+            // Camera permission has not been granted yet. Request it directly.
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA},
+                    REQUEST_CAMERA);
+        }
+        // END_INCLUDE(camera_permission_request)
     }
 
 }
